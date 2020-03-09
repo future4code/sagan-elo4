166ac97f42b150004fb5dfbacd4278eb8ee506bd
Filipe Virtuoso <lfvirtuoso@gmail.com>
2020-03-09 13:30:51 -0300

    Adicionar GlobalStyles

--- elo4/src/App.js
+++ elo4/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import GlobalStyles from './styles/global';
 
 const App = props => {
   // static.propTypes = {};
 
   return (
     <div className="App">
+      <GlobalStyles />
+
       <h1>Hello world</h1>
     </div>
   );
