f7ac7f8f254961d9c0e30ec226060378b89c0c13
WesleyAraujoBR <wesleyaraujoferminodejesus@gmail.com>
2020-03-12 16:45:42 -0300

    Conclusão das funcionalidades básicas do carrinho de compras.

--- elo4/src/App.js
+++ elo4/src/App.js
@@ -3,19 +3,102 @@ import PropTypes from 'prop-types';
 import GlobalStyles from './styles/global';
 import CarrinhoDeCompras from './Components/CarrinhoDeCompras'
 
+const BaseUrl = "https://us-central1-future-apis.cloudfunctions.net/elo4"
+
 class App extends React.Component {
   // static.propTypes = {};
+  constructor(props){
+    super(props)
+    this.state = {
+      cardCarrinho: [
+        {
+          id: "Q8cybKtKmAEQphrXccQO",
+          paymentMethod: "card",
+          photos: ["https://live.staticflickr.com/5735/31369632556_bc4884f319_b.jpg"],
+          name: "Produto 1",
+          installments: 3,
+          category: "Categoria 1",
+          price: 10,
+          description: "Esse é um produto muito legal!"
+        },
+        {
+          id: "Q8cybKtKmAEQphrXcc22",
+          paymentMethod: "card",
+          photos: ["https://live.staticflickr.com/5762/31425462216_1aeb703d38_b.jpg"],
+          name: "Produto 2",
+          installments: 3,
+          category: "Categoria 1",
+          price: 40,
+          description: "Esse é um produto muito útil para você!"
+        },
+        {
+          id: "Q8cybKtKmAEQphrXcc33",
+          paymentMethod: "card",
+          photos: ["https://live.staticflickr.com/65535/49364756227_f2d03c2b77_b.jpg"],
+          name: "Produto 3",
+          installments: 3,
+          category: "Categoria 1",
+          price: 98.55,
+          description: "Compre, compre, compre!!"
+        },
+        {
+          id: "Q8cybKtKmAEQphrXcc55",
+          paymentMethod: "card",
+          photos: ["https://live.staticflickr.com/5826/21286099678_c0aa4182a4_b.jpg"],
+          name: "Produto 4",
+          installments: 3,
+          category: "Categoria 1",
+          price: 2340,
+          description: "Esse é um produto muito legal!"
+        }
 
+      ],
+      produtos: [
+        {
+          id: "",
+          paymentMethod: "",
+          photos: "",
+          name: "",
+          installments: 0,
+          category: "",
+          price: 0,
+          description: ""
+        }, 
+      ] 
+    }
+  }
+
+apagarProdutoDoCarrinho = elem => {
+  const novoCarrinho = this.state.cardCarrinho.filter(el => el.id !== elem.id);
+  this.setState({cardCarrinho: novoCarrinho});
+};
+
+exibirCarrinho = () => {
+  this.state.exibirCarrinho === 
+  false ? this.setState({ exibirCarrinho: true }) 
+  : this.setState({ exibirCarrinho: false })
+}
+
+adicionarAoCarrinho = id => {
+  const item = this.state.produtos.find(item => {
+    return item.id === id
+  })
+
+  const novoItem = [...this.state.cardCarrinho, item]
+  this.setState({cardCarrinho: novoItem})
+}
 
   render() {
     return (
       <div className="App">
         <GlobalStyles />
-
-        <CarrinhoDeCompras />
+        <h2> Carrinho de compras </h2>
+        <hr />
+        <CarrinhoDeCompras card={this.state.cardCarrinho} delete={this.apagarProdutoDoCarrinho} />
       </div>
     );
   }
 };
 
 export default App;
+
--- elo4/src/Components/CarrinhoDeCompras.js
+++ elo4/src/Components/CarrinhoDeCompras.js
@@ -3,8 +3,6 @@ import React from 'react'
 // import styled from 'styled-components'
 // import PropTypes from 'prop-types'
 import ItemLista from './ItemLista'
-import TrocaPaginaCarrinhoComItens from './TrocaPaginaCarrinhoComItens'
-// import TrocaPaginaCarrinhoSemItens from './TrocaPaginaCarrinhoSemItens'
 
 const BaseUrl = "https://us-central1-future-apis.cloudfunctions.net/elo4"
 
@@ -12,25 +10,67 @@ class CarrinhoDeCompras extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            paginaAtual: "carrinho-teste"
+        //     cardCarrinho: [
+        //     id: "",
+        //     paymentMethod: "",
+        //     photos: "",
+        //     name: "",
+        //     installments: 0,
+        //     category: "",
+        //     price: 0,
+        //     description: ""
+        // ]
         }
     }
 
-    lidaComCarrinho = () => {
-        if(this.state.paginaAtual === "carrinho-teste"){
-            this.setState({paginaAtual: "teste2"})
-        } else{
-            this.setState({paginaAtual: "carrinho-teste"})
-        }
+
+
+    voltarPaginaInicial = () => {
 
     }
-
 
     render(){
+        let total = 0
+        const mostraItens = this.props.card.map((elem, index) => {
+            total += elem.price
+            return (
+                <div>
+                    <section>
+                        <span><img src={elem.photos} alt="Foto" /> {elem.name} {elem.description} {elem.price} </span>
+                        <span onClick={() => this.props.delete(elem)}> X </span>
+                    </section>
+                </div>
+            )
+        })
+
+        let card
+        if (this.props.card.length > 0) {
+            card = (
+                <section>
+                    <span>Produto </span>
+                    <span> Nome </span>
+                    <span> Descrição </span>
+                    <span> Preço </span>
+                    <div>{ mostraItens }</div>
+                </section>
+            )
+        } else {
+            card = (
+                <div>
+                    <h2>Você ainda não adicionou nenhum carrinho à sua lista de compras.
+                        Que tal retornar à página inicial? Há muitas opções para você! Aproveite!!
+                    </h2>
+                    <button>Voltar para a Home</button>
+                </div>
+            )
+        }
+
         return (
-            <div>
-               <TrocaPaginaCarrinhoComItens />
-            </div>
+            <section>
+               { card }
+               <hr />
+               { this.props.card.length > 0 ? <p>Valor Total: R$ {total}</p> : null }
+            </section>
         )
     }
 }
